c425a665ebcfa1eb6bb91df58ec763e6410ee48a
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2023-11-27 10:33:40 +0100

    jason changes, nothin important

--- form_gyakorlas_1/script.js
+++ form_gyakorlas_1/script.js
@@ -1,12 +1,12 @@
-function submitForm(){
+function submitForm() {
     var fullName = document.getElementById("fullName").value;
     var password = document.getElementById("password").value;
     var gender = document.getElementsByName("gender");
 
     var selectedGender = null;
 
-    for(var i = 0; i < gender.length;i++){
-        if(gender[i].checked == true)
+    for (var i = 0; i < gender.length; i++) {
+        if (gender[i].checked == true)
             selectedGender = gender[i];
     }
 
@@ -15,16 +15,16 @@ function submitForm(){
 
     var languages = document.getElementsByName("prog");
     var languagesSelected = [];
-    for(var i = 0; i < languages.length;i++){
-        if(languages[i].checked == true){
+    for (var i = 0; i < languages.length; i++) {
+        if (languages[i].checked == true) {
             languagesSelected.push(languages[i].value);
         }
     }
 
-//itt lehetne simán csak kiirattatni is viszont így letisztultabban néz ki a consoleon
-for(var i = 0; i < languagesSelected.length;i++){
-    console.log(languagesSelected[i]);
-}
+    //itt lehetne simán csak kiirattatni is viszont így letisztultabban néz ki a consoleon
+    for (var i = 0; i < languagesSelected.length; i++) {
+        console.log(languagesSelected[i]);
+    }
 
 
 
