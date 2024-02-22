5fe2669428a27c2d44426baea39f1411a94f6789
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2024-02-22 14:07:48 +0100

    javascriptBasic, uj lap az iskolai feladatok kozott

--- javascript_gyakorlas_1/script.js
+++ javascript_gyakorlas_1/script.js
@@ -1,16 +1,7 @@
 var checkbox_szam;
 var radio_szam;
 
-function cimkeBekereoLetrehozasa() {
-	/*
-	TO DO:
-	- változók létrehozása a bekért értékek tárolásához
-	- változó létrehozása az új tartalom tárolásához
-	- a bekért értékek lekérése
-	- a "masodik" div tartalmának létrehozása
-	- az "elso" div elrejtése és a "masodik" div láthatóvá tétele
-	*/
-
+function cimkeBekeroLetrehozasa() {
 	checkbox_szam = parseInt(document.getElementById('checkbox_szam').value);
 	radio_szam = parseInt(document.getElementById('radio_szam').value);
 	console.log(checkbox_szam, typeof(checkbox_szam));
@@ -33,13 +24,54 @@ function cimkeBekereoLetrehozasa() {
 		tartalom += "<option value=" + i + ">" + i + "</option>";
 	}
 	tartalom += "</select><br><br>";
-	tartalom += "<input type='button' value='Mehet' onclick='elemekLetrehozasa()'></button>";
+	tartalom += "<input type='button' value='Tovabb' onclick='elemekKiirasa()'></button>";
 	tartalom += "</form>";
 	document.getElementById("masodik").innerHTML = tartalom;
 	
 	document.getElementById("elso").style.display = "none";
 	document.getElementById("masodik").style.display = "block";
 }
+
+
+//teszt
+
+function elemekKiirasa() {
+	checkbox_szam = parseInt(document.getElementById('checkbox_szam').value);
+	radio_szam = parseInt(document.getElementById('radio_szam').value);
+	console.log(checkbox_szam, typeof(checkbox_szam));
+	console.log(radio_szam, typeof(radio_szam));
+	
+	var tartalom = "<form>";
+	var checkbox = ""
+	// checkboxok bekérése (címke és bejelölés)
+	for (i=1; i<=checkbox_szam; i++) {
+		checkbox = document.getElementById("checkbox_" + i);
+		console.log(checkbox)
+		//tartalom += "<label>A(z) " + i + ". radio gomb címkéje: <input name='radiok' id=radio_" + i + " type='text'></label><br>";
+	}
+	for (i=1; i<=checkbox_szam; i++) {
+		tartalom += "<label>A(z) " + i + ". checkbox címkéje: <input type='text' name='checkboxok' id=checkbox_" + i + "></label>";
+		tartalom += "<label> Legyen bepipálva? <input type='checkbox' name='checkboxok_checked'></label><br>";
+	}
+	// radio gombok bekérése (címke)
+	tartalom += "<br>";
+	for (i=1; i<=radio_szam; i++) {
+		tartalom += "<label>A(z) " + i + ". radio gomb címkéje: <input name='radiok' id=radio_" + i + " type='text'></label><br>";
+	}
+	// melyik radio gomb legyen kiválasztva (választó)
+	tartalom += "Hányadik radio gomb legyen az alapértelmezetten kiválasztott? <select id='radio_id_select'>";
+	for (i=1; i<=radio_szam; i++) {
+		tartalom += "<option value=" + i + ">" + i + "</option>";
+	}
+	tartalom += "</select><br><br>";
+	tartalom += "<input type='button' value='Kiiras' onclick='elemekKiirasa()'></button>";
+	tartalom += "</form>";
+	document.getElementById("harmadik").innerHTML = tartalom;
+	
+	document.getElementById("masodik").style.display = "none";
+	document.getElementById("harmadik").style.display = "block";
+}
+
 
 function elemekLetrehozasa() {
 	/*
