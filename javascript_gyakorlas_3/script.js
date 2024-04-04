7fc4718569f31056279ad6f02a24df70b2189bad
Bogat25 <68167886+Bogat25@users.noreply.github.com>
2024-04-04 13:59:07 +0200

    xml, nincs kesz

--- javascript_gyakorlas_3/script.js
+++ javascript_gyakorlas_3/script.js
@@ -5,15 +5,20 @@ fetch("test.json")
     //console.log(adat);
     // Itt dolgozhatod fel az adatokat
     legorduloMenu = document.getElementById("legorduloMenu");
-    var kontinensek = new Array;
+    var kontinensek = new Array();
     for (var i = 0; i < adat.varosok.length; i++) {
-        if(!kontinensek.includes(adat.varosok[i].kontinens)) {
-            kontinensek.push(adat.varosok[i].kontinens);
-        }
+      if (!kontinensek.includes(adat.varosok[i].kontinens)) {
+        kontinensek.push(adat.varosok[i].kontinens);
+      }
     }
-    for(var i = 0; i < kontinensek.length; i++){
-        //legorduloMenu.innerHTML += '<option value="' + kontinensek[i] +'">' + kontinensek[i] + '</option>';
-        legorduloMenu.innerHTML += '<option value="' + kontinensek[i] +'">' + kontinensek[i] + ' </option>';
+    for (var i = 0; i < kontinensek.length; i++) {
+      //legorduloMenu.innerHTML += '<option value="' + kontinensek[i] +'">' + kontinensek[i] + '</option>';
+      legorduloMenu.innerHTML +=
+        '<option value="' +
+        kontinensek[i] +
+        '">' +
+        kontinensek[i] +
+        " </option>";
     }
   })
 
@@ -41,3 +46,68 @@ function betoltesJSON() {
       console.error("Hiba történt a fájl beolvasása során: ", error)
     );
 }
+//XML-re valo lekrdezes elvalasztas
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// A legördülő menü kiválasztása
+let legorduloMenuXML = document.getElementById("legorduloMenuXML");
+
+function betoltXML() {
+    fetch("test.xml")
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((adatXML) => {
+        console.log("test");
+      const varosok = adatXML.getElementsByTagName("varos");
+      let kontinensek = [];
+
+      for (let i = 0; i < varosok.length; i++) {
+        const kontinens =
+          varosok[i].getElementsByTagName("kontinens")[0].childNodes[0]
+            .nodeValue;
+        if (!kontinensek.includes(kontinens)) {
+          kontinensek.push(kontinens);
+        }
+      }
+
+      legorduloMenuXML.innerHTML = ""; // A legördülő menü ürítése
+      kontinensek.forEach((kontinens) => {
+        legorduloMenu.innerHTML += `<option value="${kontinens}">${kontinens}</option>`;
+      });
+      legorduloMenuXML;
+    })
+    .catch((error) =>
+      console.error("Hiba történt a fájl beolvasása során: ", error)
+    );
+}
+
+function betoltesXML() {
+  fetch("test.xml")
+    .then((response) => response.text())
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((adatXML) => {
+      console.log("test");
+      const varosok = adatXML.getElementsByTagName("varos");
+      const kivalasztottKontinens =
+        document.getElementById("legorduloMenuXML").value;
+      let DivBetoltesXML = document.getElementById("containerXML");
+      DivBetoltesXML.innerHTML = ""; // A tartalom ürítése
+
+      for (let i = 0; i < varosok.length; i++) {
+        const kontinens =
+          varosok[i].getElementsByTagName("kontinens")[0].childNodes[0]
+            .nodeValue;
+        if (kivalasztottKontinens == kontinens) {
+          const orszag =
+            varosok[i].getElementsByTagName("orszag")[0].childNodes[0]
+              .nodeValue;
+          DivBetoltesXML.innerHTML += orszag + "<br>";
+        }
+      }
+    })
+    .catch((error) =>
+      console.error("Hiba történt a fájl beolvasása során: ", error)
+    );
+}
+
+// Függvények hívása az oldal betöltésekor
+betoltXML();
